5764a08690e35fcb987f60d4dff9ffbed5b1ba05
braian-valor <braian.valor@alumnos.frgp.utn.edu.ar>
2023-10-28 16:48:03 -0300

    Clase de AccesoDatos agregada

---
+++ Data/AccesoDatos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conecion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conecion = new SqlConnection("Cadena de conexion..");
+            comando = new SqlCommand();
+        }
+
+        public void setQuery(string query)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = query;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conecion;
+            try
+            {
+                if (conecion.State != System.Data.ConnectionState.Open) 
+                    conecion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conecion.Close();
+        }
+
+        public void setParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void setProcedure(string sp)
+        {
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conecion;
+            try
+            {
+                if (conecion.State != System.Data.ConnectionState.Open)
+                    conecion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- Data/Properties/AssemblyInfo.cs
+++ Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("AccesoDatos")]
+[assembly: AssemblyTitle("Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccesoDatos")]
+[assembly: AssemblyProduct("Data")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("16730b72-972b-4eed-95e4-5a2a360eafe4")]
+[assembly: Guid("a97fa27c-d2bb-4bcc-a6c0-ec4a2d58f743")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
