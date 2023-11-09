5ccd6d3df4528d41bb21b035d063e7203137ef26
braian-valor <braian.valor@alumnos.frgp.utn.edu.ar>
2023-11-09 14:13:03 -0300

    clases Insumo, Categoria y InsumoNegocio agregadas

---
+++ Dominio/Insumo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Insumo
+    {
+        public int Id { get; set; }
+        public Categoria Categoria { get; set; }
+        public string Nombre { get; set; }
+        public int Stock { get; set; }
+        public int StockMinimo { get; set; }
+        public decimal Precio { get; set; }
+        public int Estado { get; set; }
+
+    }
+}
